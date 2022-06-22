8eedbda119c0908be0facfcae4a50788cc009e59
Faiydee <m.farazjaved166@gmail.com>
2022-06-22 02:11:44 +0200

    changed normal navbar to reponsive

--- firsttry/src/Components/Navbar.js
+++ firsttry/src/Components/Navbar.js
@@ -1,31 +1,46 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './Navbar.css';
 
 const Navbar = () => {
     return(
-        <div>
-            <nav>
-        <div className='menu'>
-          <div className="ui inverted menu">
-            <a className="item">
-              <Link to = '/'>
-                Home
-              </Link>
-            </a>
-            <a className="item">
-              <Link to = '/district'>
-                Back
-              </Link>
-            </a>
-            <a className="item">
-              <Link to = '/uploadImage'>
-                Upload Image
-              </Link>
-            </a>
+        <>
+        <nav className="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+    <a class="navbar-brand" href="#"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <div className='menu'>
+                    <div className="navbar">
+                      <div className="ui inverted menu">
+                        <li>
+                          <Link to = '/'>
+                            <a className="item">Home</a>
+                          </Link>
+                        </li>
+                        <li>
+                          <Link to = '/district'>
+                            <a className="item">Districts</a>
+                          </Link>
+                        </li>
+                        <li>
+                        <Link to = '/uploadImage'>
+                          <a className="item"> Upload Image </a>
+                        </Link>
+                        </li>
+                      </div>
+                    </div>
+                </div> 
+
+
+              </ul>
+             </div>
           </div>
-        </div> 
-      </nav>
-        </div>
+        </nav>
+        </>
     )
 };
 
